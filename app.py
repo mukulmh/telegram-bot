50ffe26e71b4b74ac241bc9637f5bc7266883dce
mukulmh <mukulseu@gmail.com>
2024-05-04 14:33:16 +0600

    base app for sending group message

--- app.py
+++ app.py
@@ -1,40 +1,8 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from celery_config import send_message_to_group
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-import asyncio, uvicorn
 
 app = FastAPI()
-
-bot_token = '7079362195:AAHcxwVQHrsVfVvpDJbNOn0_LCttV0gl0zs'
-
-application = Application.builder().token(bot_token).build()
-application.initialize()  # Initialize the Application instance
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.effective_chat.id
-    print(chat_id)
-    # Send a welcome message back to the user
-    await context.bot.send_message(chat_id=chat_id, text="Welcome to the bot!")
-
-start_handler = CommandHandler('start', start)
-application.add_handler(start_handler)
-
-@app.post("/webhook")
-async def handle_webhook(request: Request):
-    # Parse the JSON request body
-    data = await request.json()
-    print(data)
-    # Convert the data into a Telegram Update
-    update = Update.de_json(data, application.bot)
-    print(update)
-
-    # Use the application to process the update
-    await application.update_queue.put(update)
-    
-    # Return a simple JSON response to acknowledge receipt of the update
-    return {"status": "ok"}
 
 class MessageData(BaseModel):
     api_token: str
@@ -51,23 +19,6 @@ async def handle_send_message(data: MessageData):
 @app.get("/")
 async def home(request: Request):
     return {"message": "Success"}
-
-# Main function to start both FastAPI and Telegram bot
-def main():
-    # Run the FastAPI server using uvicorn.run
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-# Start the application
-if __name__ == "__main__":
-    # Run the FastAPI server in a thread or as a separate task
-    # depending on your server and environment
-
-    # Use application.run_webhook() directly
-    # Update host, port, and path as needed
-    application.run_webhook(listen="0.0.0.0", port=8000, url_path="/webhook")
-
-    # Run the FastAPI server after the Telegram bot is set up
-    main()
 
 # To run the FastAPI application, use the following command in the terminal:
 # uvicorn app:app --host 0.0.0.0 --port 8000
