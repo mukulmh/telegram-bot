e7dcaff0582d94259849c1cc932fa00e4df83649
mukulmh <mukulseu@gmail.com>
2024-05-08 10:19:07 +0600

    webhook app update

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from flask import Flask, request
 import telegram
 import os
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 bot_token = os.environ.get('BOT_TOKEN')
-
 app = Flask(__name__)
 bot = telegram.Bot(token=bot_token)
+
+
+def run_coroutine_sync(coro):
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(coro)
+
 
 @app.route('/webhook', methods=['POST'])
 def respond():
@@ -17,25 +23,18 @@ def respond():
     print("got text message :", text)
 
     if text == '/start':
+        print("start command received")
         welcome_text = 'Welcome to the bot! Type /help to see the available commands.'
-        bot.sendMessage(chat_id=chat_id, text=welcome_text)
+        run_coroutine_sync(bot.send_message(chat_id=chat_id, text=welcome_text))
     elif text == '/help':
+        print("help command received")
         help_text = 'Available commands:\n/start - Start the bot\n/help - Show the available commands'
-        bot.sendMessage(chat_id=chat_id, text=help_text)
+        run_coroutine_sync(bot.send_message(chat_id=chat_id, text=help_text))
     else:
-        bot.sendMessage(chat_id=chat_id, text='Invalid command. Type /help to see the available commands.')
+        print("invalid command received")
+        run_coroutine_sync(bot.send_message(chat_id=chat_id, text='Invalid command. Type /help to see the available commands.'))
         
     return 'ok'
-
-
-@app.route('/setwebhook', methods=['GET', 'POST'])
-def set_webhook():
-    s = bot.setWebhook('https://telegram-bot-f9vw.onrender.com/webhook')
-    print(s)
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
 
 
 @app.route('/')
